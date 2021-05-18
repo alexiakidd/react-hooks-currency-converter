5bc9c93de749a01b1c1bd09f2dc5b0bcde617589
Alessandra Lima <alexiakidd.dev@gmail.com>
2021-05-18 00:50:51 -0300

    feat: Creating input text

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,7 +1,27 @@
 import './conversor-moedas.css'
+import { Jumbotron, Button, Form, Col, Spinner } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
 
 function ConversorMoedas() {
-	return <h1>Conversor moedas</h1>
+	return (
+		<div>
+			<h1>Conversor moedas</h1>
+			<Jumbotron>
+				<Form>
+					<Form.Row>
+						<Col sm="3">
+							<Form.Control placeholder="0" value={1} required />
+						</Col>
+						<Col sm="3"></Col>
+						<Col sm="1"></Col>
+						<Col sm="3"></Col>
+						<Col sm="2"></Col>
+					</Form.Row>
+				</Form>
+			</Jumbotron>
+		</div>
+	)
 }
 
 export default ConversorMoedas
